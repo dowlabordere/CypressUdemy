7b5e2173b5a15f25017b6ff1aeeae4e3c7180f65
dowlabordere <vjvladakorn@yahoo.com>
2020-06-03 09:41:42 +0200

    Update Popup.js

--- integration/examples/Popup.js
+++ integration/examples/Popup.js
@@ -9,7 +9,7 @@ describe('My first Test Suite', function () {
         // popups
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
         cy.get('#displayed-text').type(name).then(function(){
-            cy.wait(2000)
+            cy.wait(3000)
             cy.get('#alertbtn').click()
         })
         cy.get('[value="Confirm"]').click()
